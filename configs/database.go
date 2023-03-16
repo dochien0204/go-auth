28d047b781365cc1b838246e40146fc089ba7449
dochien0204 <doxuanchienh02042002@gmail.com>
2023-03-16 08:58:56 +0700

    Update docker-compose.yml

--- configs/database.go
+++ configs/database.go
@@ -17,7 +17,7 @@ func ConnectDB() {
 	username := "postgres"
 	password := "postgres"
 	databaseName := "users"
-	port := "5432"
+	port := "5400"
 
 	dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, username, password, databaseName, port)
 
