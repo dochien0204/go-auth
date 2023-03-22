25cf9a83bd6d70969e74e8567b5dddfb684d5bc3
dochien0204 <doxuanchienh02042002@gmail.com>
2023-03-22 16:53:06 +0700

    Login and Register

---
+++ pkg/auth/client.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"Microservices/pkg/auth/pb"
+	"Microservices/pkg/config"
+	"fmt"
+
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.AuthServicesClient
+}
+
+func InitServiceClient(c *config.Config) pb.AuthServicesClient {
+
+	client, err := grpc.Dial(c.AuthSvcUrl, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("Cannot connect:", err)
+	}
+
+	return pb.NewAuthServicesClient(client)
+}
